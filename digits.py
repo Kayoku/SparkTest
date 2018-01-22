3f957fcd4ac0cf851a672d50c21ca479e45829df
Deleruyelle Arnaud <deleruyelle@b04p6.fil.univ-lille1.fr>
2018-01-22 16:48:31 +0100

    la folie

--- digits.py
+++ digits.py
@@ -1,6 +1,8 @@
 from sklearn import datasets
 from pyspark import *
 from pyspark.sql import *
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import NaiveBayes
 from pyspark.ml.linalg import Vectors
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.classification import MultilayerPerceptronClassifier
@@ -12,7 +14,7 @@ digits = datasets.load_digits()
 data = digits.data[100:]
 labels = digits.target[100:]
 
-# 1797 x 2 
+# 1797 x 2
 datafr = []
 for i in range(len(data)):
     datafr.append((int(labels[i]), Vectors.dense(data[i].tolist())))
@@ -20,13 +22,30 @@ for i in range(len(data)):
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 
-set_training = sqlContext.createDataFrame(datafr, ["label", "features"])
-lr = LogisticRegression(maxIter=3000, regParam=0.1)
-layers = [4, 15, 15, 15, 10]
-lrNL = MultilayerPerceptronClassifier(maxIter=100, layers=layers, blockSize=128, seed=1234)
+# train data
+train = sqlContext.createDataFrame(datafr, ["labels", "images"])
+train.show(20)
 
-lrModelNL = lrNL.fit(set_training)
-lrModel = lr.fit(set_training)
+
+
+lr = LogisticRegression(maxIter=3000, regParam=0.1,labelCol="labels",rawPredictionCol = "rPredict",featuresCol = "images",predictionCol = "p_nl",probabilityCol = "proba_nl")
+
+"""
+#multilayer perceptron
+layers = [4, 15, 15, 15, 10]
+lrNL = MultilayerPerceptronClassifier(maxIter=100, layers=layers)
+"""
+
+
+# bayes classifier
+nb = NaiveBayes(smoothing=1.0, modelType="multinomial", predictionCol="p_nb", featuresCol = "images",labelCol="labels")
+
+
+# create pipeline and fit
+pipeline = Pipeline(stages=[lr,nb])
+model = pipeline.fit(train)
+
+
 
 data_test = digits.data[:100]
 labels_test = digits.target[:100]
@@ -36,10 +55,14 @@ datatest = []
 for i in range(len(data_test)):
     datatest.append((int(labels_test[i]), Vectors.dense(data_test[i].tolist())))
 
-set_test = sqlContext.createDataFrame(datatest, ["label", "features"])
-prediction = lrModel.transform(set_test)
-predictionNL = lrModelNL.transform(set_test)
+set_test = sqlContext.createDataFrame(datatest, ["labels", "images"])
 
+
+
+# transform on test
+prediction = model.transform(set_test)
+prediction.show(50)
+"""
 probas = []
 selected = prediction.select("label", "prediction")
 selectedNL = predictionNL.select("label", "prediction")
@@ -56,3 +79,4 @@ for row in selectedNL.collect():
     if prediction == lbl:
         probas[0]+=1
 print(probas)
+"""
